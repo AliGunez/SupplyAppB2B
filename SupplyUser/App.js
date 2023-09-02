485eb32595992a9f64a58c57dd5e2254be987d76
AliGunez <gunesali97@gmail.com>
2023-09-02 18:20:15 +0100

    Supplier Item Component

--- SupplyUser/App.js
+++ SupplyUser/App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 import SupplierItem from './src/components/SupplierItem';
 import Suppliers from "./assets/data/Suppliers.json"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Supplier Item*/}
-      <SupplierItem supplier={Suppliers[0]} />
-      <SupplierItem supplier={Suppliers[1]} />
-      <SupplierItem supplier={Suppliers[2]} />
+      <FlatList
+       data={Suppliers}
+       renderItem={({item}) => <SupplierItem supplier={item} />}
+      />
 
       <StatusBar style="auto" />
     </View>
--- SupplyUser/src/components/SupplierItem/index.js
+++ SupplyUser/src/components/SupplierItem/index.js
@@ -1,19 +1,21 @@
 import { StyleSheet, Text, View, Image} from 'react-native';
 
 const SupplierItem = ({ supplier }) => {
-  return(
-    <View style={styles.supplierContainer}>
-    <Image 
-    source={{
-      uri: supplier.image,
-      }} style={styles.image}
-      />
-      <Text style={styles.title}>{supplier.name}</Text>
-      <Text style={styles.subtitle}>
-        £{supplier.deliveryFee} &#8226; {supplier.minDeliveryTime}-{supplier.maxDeliveryTime} days</Text>
-  </View>
+    return (
+        <View style={styles.supplierContainer}>
+            <Image
+                source={{
+                    uri: supplier.image,
+                }}
+                style={styles.image} 
+            />
+            <Text style={styles.title}>{supplier.name}</Text>
+            <Text style={styles.subtitle}>
+                £{supplier.deliveryFee} &#8226; {supplier.minDeliveryTime}-{supplier.maxDeliveryTime} days
+            </Text>
+        </View>
 
-  );
+    );
 };
 
 export default SupplierItem;
@@ -25,7 +27,7 @@ const styles = StyleSheet.create({
     },
     image: {
       width: "100%",
-      aspectRatio: 5/3,
+      aspectRatio: 6/3,
       marginBottom: 3,
     },
     title: {
