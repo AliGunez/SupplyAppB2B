38a1c614682fa7e692c1e6d560ca4aaae6220e84
AliGunez <gunesali97@gmail.com>
2023-09-08 17:21:40 +0100

    Create ItemDeatails Page

---
+++ SupplyUser/src/screens/ItemDetailsScreen/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from "react-native";
+
+const ItemDetailsScreen = () => {
+    return (
+        <View>
+            <Text>Item Details</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ItemDetailsScreen;
