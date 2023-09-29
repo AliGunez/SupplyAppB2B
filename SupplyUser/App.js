7b73cd60f0b96fda48b16f4ddd0bc1661fac35f1
AliGunez <gunesali97@gmail.com>
2023-09-29 22:09:01 +0100

    aws-amplify-react-native
    bug fix

--- SupplyUser/App.js
+++ SupplyUser/App.js
@@ -3,8 +3,13 @@ import RootNavigator from './src/navigation';
 
 
 import { NavigationContainer } from '@react-navigation/native';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from "aws-amplify-react-native"
+import config from './src/aws-exports';
 
-export default function App() {
+Amplify.configure(config);
+
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator/>
@@ -13,3 +18,5 @@ export default function App() {
     </NavigationContainer>
   );
 };
+
+export default withAuthenticator(App);
