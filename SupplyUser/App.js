11ee5d91ee9dbcc5c01303efd8a2123cf929ce5d
AliGunez <gunesali97@gmail.com>
2023-10-04 23:44:43 +0100

    Update App.js

--- SupplyUser/App.js
+++ SupplyUser/App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import RootNavigator from './src/navigation';
-
-
 import { NavigationContainer } from '@react-navigation/native';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from "aws-amplify-react-native"
 import config from './src/aws-exports';
 
 Amplify.configure({...config, Analytics: {disabled: true}});
+
 
 function App() {
   return (
@@ -18,5 +17,6 @@ function App() {
     </NavigationContainer>
   );
 };
+
 
 export default withAuthenticator(App);
